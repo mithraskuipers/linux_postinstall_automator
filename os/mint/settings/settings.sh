8379ab953c5c7e242d7f6bc0c5052e5a0df9f6ad
Mithras Kuipers <mithraskuipers@gmail.com>
2022-11-12 14:36:09 +0100

    Added automated configuration and added some more software to install

--- os/mint/settings/settings.sh
+++ os/mint/settings/settings.sh
@@ -1,4 +1,51 @@
 #!/bin/bash
 
-echo "There are no settings to automate yet..."
+cfg_path=$(echo $(pwd))
 
+
+################################################################################
+# Application installation commands                                            #
+################################################################################
+
+# YTDLP
+func_cfg_ytdlp()
+{
+	cfg_path_ytdlp_destination=$(echo ~/.config/yt-dlp) ;
+	mkdir -p $cfg_path_ytdlp_destination ;
+	cfg_path_ytdlp=${cfg_path}/os/shared/yt-dlp ;
+	cfg_path_ytdlp_source=${cfg_path_ytdlp}/configs ;
+	cfg_path_ytdlp_source_config=${cfg_path_ytdlp_source}/config ;
+	cfg_path_ytdlp_source_config_comments=${cfg_path_ytdlp_source}/config_comments ;
+	cfg_path_ytdlp_source_config_audio=${cfg_path_ytdlp_source}/config_audio ;
+	cp $cfg_path_ytdlp_source_config $cfg_path_ytdlp_destination ;
+	cp $cfg_path_ytdlp_source_config_comments $cfg_path_ytdlp_destination ;
+	cp $cfg_path_ytdlp_source_config_audio $cfg_path_ytdlp_destination ;
+	echo 'alias yt="yt-dlp"' >> ~/.bashrc ;
+	echo 'alias yta="yt-dlp --config-location ~/.config/yt-dlp/config_audio"' >> ~/.bashrc ;
+	echo 'alias ytc="yt-dlp --config-location ~/.config/yt-dlp/config_comments"' >> ~/.bashrc
+}
+
+################################################################################
+# Menu settings                                                                #
+################################################################################
+
+app_name_ytdlp="ytdlp"
+menu_title="Select\ software\ to\ automatically\ configure"
+menu_options="FALSE ytdlp"
+
+################################################################################
+# Show the menu                                                                #
+################################################################################
+
+pwd_base=$(echo $(pwd))
+pwd_base=${pwd_base}"/os/mint"
+source ${pwd_base}/installer_config.sh
+app_menu_choices=$(eval "$menu_template")
+
+################################################################################
+# Install selected apps                                                        #
+################################################################################
+
+if grep -q "$app_name_ytdlp" <<< "$app_menu_choices"; then
+	func_cfg_ytdlp
+fi
--- os/mint/software/software.sh
+++ os/mint/software/software.sh
@@ -25,15 +25,53 @@ app_cmd_element="echo $PASSWORD | \
 	sudo apt update | \
 	sudo apt install element-desktop -y"
 
+# GIT
+app_cmd_git="echo $PASSWORD | \
+	sudo apt install git -y"
+
+# MAKE
+app_cmd_make="echo $PASSWORD | \
+	sudo apt install make -y"
+
+# GCC
+app_cmd_gcc="echo $PASSWORD | \
+	sudo apt install gcc -y"
+
+# CLANG
+app_cmd_clang="echo $PASSWORD | \
+	sudo apt install clang -y"
+
+# GEDIT
+app_cmd_clang="echo $PASSWORD | \
+	sudo apt install gedit -y"
+
+# YTDLP
+app_cmd_ytdlp="echo $PASSWORD | \
+	sudo apt install yt-dlp -y ;
+	sudo apt-get install aria2 -y"
+
 ################################################################################
 # Menu settings                                                                #
 ################################################################################
 
 app_name_element="Element"
 app_name_brave="Brave"
+app_name_git="Git"
+app_name_make="Make"
+app_name_gcc="GCC"
+app_name_clang="clang"
+app_name_clang="gedit"
+app_name_ytdlp="ytdlp"
 
-menu_title="selection"
-menu_options="TRUE Brave TRUE Element"
+menu_title="Select\ software\ to\ automatically\ install"
+menu_options="	TRUE Brave\
+				TRUE Element\
+				TRUE Git\
+				TRUE Make\
+				TRUE GCC\
+				TRUE clang\
+				TRUE ytdlp\
+				TRUE gedit"
 
 ################################################################################
 # Show the menu                                                                #
@@ -48,10 +86,34 @@ app_menu_choices=$(eval "$menu_template")
 # Install selected apps                                                        #
 ################################################################################
 
+if grep -q "$app_name_brave" <<< "$app_menu_choices"; then
+	echo $app_cmd_brave | bash
+fi
+
 if grep -q "$app_name_element" <<< "$app_menu_choices"; then
 	echo $app_cmd_element | bash
 fi
 
-if grep -q "$app_name_brave" <<< "$app_menu_choices"; then
-	echo $app_cmd_brave | bash
+if grep -q "$app_name_git" <<< "$app_menu_choices"; then
+	echo $app_cmd_git | bash
+fi
+
+if grep -q "$app_name_make" <<< "$app_menu_choices"; then
+	echo $app_cmd_make | bash
+fi
+
+if grep -q "$app_name_gcc" <<< "$app_menu_choices"; then
+	echo $app_cmd_gcc | bash
+fi
+
+if grep -q "$app_name_clang" <<< "$app_menu_choices"; then
+	echo $app_cmd_clang | bash
+fi
+
+if grep -q "$app_name_gedit" <<< "$app_menu_choices"; then
+	echo $app_cmd_gedit | bash
+fi
+
+if grep -q "$app_name_ytdlp" <<< "$app_menu_choices"; then
+	echo $app_cmd_ytdlp | bash
 fi
